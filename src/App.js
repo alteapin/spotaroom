3d7442efcbcad5a14366116e65fe862dfde8f4ae
alteapin <alteap18@gmail.com>
2019-03-19 13:08:48 +0100

    calling API and saving data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data : [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('api/homecards')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  
   render() {
     return (
       <div className="App">
-
       </div>
     );
   }
 }
-
 export default App;
