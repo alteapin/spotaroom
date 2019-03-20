7e74bb964ea9f7f71f8ae426de479b94adf82625
alteapin <alteap18@gmail.com>
2019-03-20 19:20:57 +0100

    last changes version I

---
+++ docs/build/precache-manifest.4080a434f0d2b4719edee985de8ece91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "aee2ffdb233d955e3f0c",
+    "url": "./static/js/main.f4e7f282.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "./static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "aee2ffdb233d955e3f0c",
+    "url": "./static/css/main.bd4d21fb.chunk.css"
+  },
+  {
+    "revision": "25bacfe5272a156774fa0e7a7206e66b",
+    "url": "./index.html"
+  }
+];
