4208f422f7b93b324ef6cfe1d59ac3b2274c62e9
alteapin <alteap18@gmail.com>
2019-03-20 18:19:01 +0100

    publishing in github pages

---
+++ docs/precache-manifest.13a8675d7dee7beea6b84fc606cc56d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "43b7ddb772c6659ca9d1",
+    "url": "./static/js/main.f4e7f282.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "./static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "43b7ddb772c6659ca9d1",
+    "url": "./static/css/main.5c981b58.chunk.css"
+  },
+  {
+    "revision": "9a64c0a82da502fc9ed52b086a014d32",
+    "url": "./index.html"
+  }
+];
