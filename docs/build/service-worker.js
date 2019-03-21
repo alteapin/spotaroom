e9c57b34f560a13c37c25a8ae2828bc6a4d9f9cb
alteapin <alteap18@gmail.com>
2019-03-21 10:28:35 +0100

    publising in github pages

--- docs/build/service-worker.js
+++ docs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4080a434f0d2b4719edee985de8ece91.js"
+  "./precache-manifest.a433a50884697de6818c08aa745e6484.js"
 );
 
 workbox.clientsClaim();
